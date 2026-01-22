69ce8dd17fbf34505573e626aa8bab1e93816722
Eliecer8754 <eliecerperez200313@gmail.com>
2026-01-21 20:26:11 -0500

    project completed2

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,33 @@
-import express from 'express'
+import express from "express"
 import dotenv from "dotenv"
-import { connectDB } from './config/db.js'
-import productRoutes from "./routes/product.route.js";
+import { connectDB } from "./config/db.js"
+import productRoutes from "./routes/product.route.js"
 import path from "path"
+import { fileURLToPath } from "url"
+
 dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const _dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+app.use(express.json())
 
-app.use(express.json()) //allows us to accept json data in the req.body
+app.use("/api/products", productRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-
-app.use("/api/products", productRoutes);
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(_dirname, "/frontend/dist")))
-    app.get(/.*/, (req,res)=>{
-        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-    })
+  app.get("/*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    )
+  })
 }
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log("server running in PORT:", PORT)
+app.listen(PORT, () => {
+  connectDB()
+  console.log("server running in PORT:", PORT)
 })
-
-
